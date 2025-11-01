763618aeff486a94a438efc9531b23a3f163f5f8
dushan loku liyanage <malingadushan27@gmail.com>
2025-11-01 09:10:48 +0530

    Update

--- class.php
+++ class.php
@@ -31,6 +31,71 @@
         }
     }
 
+    $classes  = get_declared_classes();
+
+    echo '<pre>';
+    print_r($classes);
+    echo '</pre>';
+
+
+
+    $name  =  '0';
+    if (class_exists($name)) {
+        echo "Class was  '.$name.' is was Avalibel";
+    } else {
+        echo "Class was $name is not was Avalibel";
+    }
+
+
+    class emplyooi
+    {
+        var $id;
+        var $fname;
+        var $lname;
+        var $is_active = false;
+    }
+
+    $empyooi_1 = new emplyooi;
+
+    $empyooi_1->id = 10;
+    $empyooi_1->fname = 'Dushan';
+    $empyooi_1->lname = 'Malinga';
+    $empyooi_1->is_active = 1;
+
+    echo ' <h2>This is empylooi Datails </h2>';
+    echo '<br>';
+    echo 'Id: ' . $empyooi_1->id;
+    echo '<br>';
+    echo 'First Name: ' . $empyooi_1->fname;
+    echo '<br>';
+    echo 'Last Name :' . $empyooi_1->lname;
+    echo '<br>';
+    echo 'Status : ' . $empyooi_1->is_active;
+
+    $list_var = get_class_vars('emplyooi');
+
+    echo ' <h3>Class Variables </h3>';
+    echo '<br>';
+    echo '<pre>';
+    print_r($list_var);
+    echo '</pre>';
+
+
+
+        $list_var = get_object_vars($empyooi_1);
+
+    echo ' <h3>Class  </h3>';
+    echo '<br>';
+    echo '<pre>';
+    print_r($list_var);
+    echo '</pre>';
+
+    if(property_exists('emplyooi','lname')){
+        echo "Goood";
+    }else{
+          echo "Bad";
+    }
+
     ?>
 
 
